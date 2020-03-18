57db4390c2f5b74ae4b8e3b1adf42b35e6d271a8
Dawn Ho <dawn.ho@gmail.com>
2020-03-18 02:56:17 -0700

    Sorted out Dependency Madness for installing LCD Display Support

---
+++ i2c/scripts/install-sdl.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+apt-get update && apt-get install -yq \
+  python3-dev \
+  python3-opengl \
+  python3-numpy \
+  python3-scipy \
+  libsdl2-dev \
+  libsdl2-2.0-0 \
+  libwebp-dev \
+  libsdl2-image-dev \
+  libsdl2-image-2.0-0 \
+  libmikmod-dev \
+  libfishsound1-dev \
+  libsmpeg-dev \
+  liboggz2-dev \
+  libflac-dev \
+  libfluidsynth-dev \
+  libsdl2-mixer-dev \
+  libsdl2-mixer-2.0-0 \
+  libfreetype6-dev \
+  libsdl2-ttf-dev \
+  libsdl2-ttf-2.0-0 \
+  xfonts-base \
+  xfonts-100dpi \
+  xfonts-75dpi \
+  xfonts-cyrillic \
+  fontconfig \
+  fonts-freefont-ttf \
+  libfreetype6-dev \
+  libportmidi-dev \
+  libswscale-dev \
+  libavformat-dev \
+  libavcodec-dev \
+  libx11-6 \
+  libx11-dev \
+  fluid-soundfont-gm \
+  timgm6mb-soundfont \
+  libsdl-mixer1.2-dev \
+  libsdl-ttf2.0-dev \
+  libsmpeg-dev \
+  libjpeg-dev \
+  libtiff5 \
+  libsdl-image1.2-dev
